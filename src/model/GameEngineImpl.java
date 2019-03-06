b078e5d36a3b28b2dc45f6d83191877f7c5ce268
David Ping <davidping96@gmail.com>
2019-03-07 09:29:27 +1100

    Added comments from interface

--- src/model/GameEngineImpl.java
+++ src/model/GameEngineImpl.java
@@ -6,59 +6,151 @@ import model.interfaces.Player;
 import model.interfaces.Slot;
 import view.interfaces.GameEngineCallback;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 public class GameEngineImpl implements GameEngine {
+
+    private List<Player> players = new ArrayList<>();
+
+    private List<GameEngineCallback> gameEngineCallbacks = new ArrayList<>();
 
     public GameEngineImpl() {
         //Empty for now
     }
 
+    /**
+     * <pre>
+     * spin the gaming wheel progressing from the initialDelay to the finalDelay
+     * in increments of delayIncrement
+     *
+     * <b>NOTE:</b> All delays are in milliseconds (ms)
+     *
+     * 1. begin by selecting a random starting slot on the wheel
+     * 2. start at initialDelay then increment the delay each time a new slot is passed on the wheel
+     * 3. call GameEngineCallback.nextSlot(...) each time a slot is passed
+     * 4. continue until delay {@literal >=} finalDelay
+     * 5. call GameEngineCallback.result(...) to finish and apply betting results
+     *
+     * <b>See Also:</b>
+     *  {@link GameEngineCallback#nextSlot(Slot slot, GameEngine engine)}
+     *  {@link GameEngineCallback#result(Slot result, GameEngine engine)}
+     *
+     * @param initialDelay
+     *            the starting delay in ms between updates
+     *            (based on how fast the ball is rolling in the slots)
+     * @param finalDelay
+     *            the final delay in ms between updates when the ball stops rolling
+     * @param delayIncrement
+     *            how much the ball slows down (i.e. delay gets longer) after each slot
+     * </pre>
+     */
     @Override
     public void spin(int initialDelay, int finalDelay, int delayIncrement) {
 
     }
 
+    /**
+     * Iterate through players and apply win/loss point balances (via {@link BetType#applyWinLoss(Player player, Slot winSlot)})
+     *
+     * @param winningSlot - the winning slot as passed to GameEngineCallback.result(...)
+     */
     @Override
     public void calculateResult(Slot winningSlot) {
 
     }
 
+    /**
+     *  <b>NOTE:</b> playerID is unique and if another player with same id is added
+     *  it replaces the previous player
+     *
+     * @param player - to add to game
+     */
     @Override
     public void addPlayer(Player player) {
-
+        Player existingPlayer = null;
+        for(Player p : players) {
+            if (p.getPlayerId().equals(player.getPlayerId())) {
+                existingPlayer = player;
+            }
+        }
+        players.remove(existingPlayer);
+        this.players.add(player);
     }
 
+    /**
+     * @param id - id of player to retrieve (null if not found)
+     * @return - the Player or null if Player does not exist
+     */
     @Override
     public Player getPlayer(String id) {
         return null;
     }
 
+    /**
+     * @param player - to remove from game
+     * @return - true if the player existed and was removed
+     */
     @Override
     public boolean removePlayer(Player player) {
         return false;
     }
 
+    /**
+     * @param gameEngineCallback
+     * <pre> a client specific implementation of GameEngineCallback used to perform display updates etc.
+     * <b>NOTE:</b> you will use a different implementation of the GameEngineCallback
+     *       for the console (assignment 1) and GUI (assignment 2) versions</pre>
+     * @see view.interfaces.GameEngineCallback
+     */
     @Override
     public void addGameEngineCallback(GameEngineCallback gameEngineCallback) {
 
     }
 
+    /**
+     * @param gameEngineCallback - instance to be removed if no longer needed
+     * @return true - if the gameEngineCallback existed
+     * @see view.interfaces.GameEngineCallback
+     */
     @Override
     public boolean removeGameEngineCallback(GameEngineCallback gameEngineCallback) {
         return false;
     }
 
+    /**
+     * @return - an unmodifiable collection (or a shallow copy) of all Players
+     * @see model.interfaces.Player
+     */
     @Override
     public Collection<Player> getAllPlayers() {
         return null;
     }
 
+    /**
+     * the implementation should make appropriate calls on the Player to place the bet and set bet type
+     *
+     * @param player - the Player who is placing the bet
+     * @param bet - the bet in points
+     * @param betType - the type of bet (red, black or either zero)
+     * @return true - if bet is greater than 0 and player had sufficient points to place the bet
+     */
     @Override
     public boolean placeBet(Player player, int bet, BetType betType) {
         return false;
     }
 
+    /**
+     * <pre> A debug method to return a FULL representation of the 38 slot gaming wheel
+     * in the order provided by Basic_roulette_wheel.png (clockwise starting at Green '00')
+     *
+     * <b>NOTE:</b> this method should also be useful in your implementation
+     * i.e. don't "reinvent the wheel" so to speak :)</pre>
+     *
+     * @return any java.util.Collection of Slot
+     * @see model.interfaces.Slot
+     */
     @Override
     public Collection<Slot> getWheelSlots() {
         return null;
--- src/model/SlotImpl.java
+++ src/model/SlotImpl.java
@@ -19,37 +19,68 @@ public class SlotImpl implements Slot {
         this.number = number;
     }
 
+    /**
+     * @return - the position of this slot (clockwise starting from position 0 for Slot GREEN00)
+     */
     @Override
     public int getPosition() {
         return this.position;
     }
 
+    /**
+     * @return - the numeric value of this slot as displayed on the gaming wheel
+     */
     @Override
     public int getNumber() {
         return this.number;
     }
 
+    /**
+     * @return - the color of this slot based on {@link Color}
+     */
     @Override
     public Color getColor() {
         return this.color;
     }
 
+    /**
+     * @return <pre> A human readable String that lists the values of this WheelSlot instance (see OutputTrace.txt)
+     *
+     * <b>NOTE:</b> Must use "proper naming" case i.e. First letter capitalised
+     * e.g. "Position: 0, Color: Green00, Number: 0" for "00" slot at top of wheel</pre>
+     */
     @Override
     public String toString() {
         return String.format("Position: %d, Color: %s, Number: %d", this.position, this.color, this.number);
     }
 
+    /**
+     * @param slot - another Slot to compare with
+     * @return - true if the color and number fields are equal
+     */
     @Override
     public boolean equals(Slot slot) {
         return (this.number == slot.getNumber() && this.color == slot.getColor());
     }
 
+    /**
+     * <pre> <b>NOTE:</b> this is the java.lang.Object @Override
+     *
+     * its implementation should cast and call through to the type checked method above</pre>
+     *
+     * @param slot - another Slot to compare with
+     * @return - true if the slot values are equal according to above equals method
+     */
     @Override
     public boolean equals(Object slot) {
         return (slot instanceof SlotImpl && this.equals((SlotImpl)slot));
     }
 
     /**
+     * <b>NOTE:</b> if equals() is true then generated hashCode should also be equal
+     *
+     * @return - generated hash code (used by various JCF Collections)
+     *
      * The uniqueness of a slot is determined by the number and the color,
      * (as seen above in the equals(Slot slot) method).
      *
